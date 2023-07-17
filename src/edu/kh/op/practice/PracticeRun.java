73d2da4e2f86e03588dba8db3e470982c8eb616f
ahnjaeyoun <vnm1994wodbs@gmail.com>
2023-07-17 15:45:23 +0900

    operator

---
+++ src/edu/kh/op/practice/PracticeRun.java
@@ -0,0 +1,16 @@
+package edu.kh.op.practice;
+
+public class PracticeRun {
+	
+	public static void main(String[] args) {
+	
+	OperatorPractice ex = new OperatorPractice();
+	
+	//ex.practice1();
+	//ex.practice2();
+	ex.practice3();
+	
+	
+
+}
+     }
